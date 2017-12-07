9566ca173026047f87b3a7be4c810eab7e1c05cd
Tanay Soni <tanaysoni12@gmail.com>
2017-12-07 19:56:08 +0530

    add models

--- know_your_ward/surveys/models.py
+++ know_your_ward/surveys/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    ward = models.CharField(max_length=10)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+
+
+class SurveyResponse(models.Model):
+    survey_id = models.ForeignKey(Survey)
+    user_id = models.ForeignKey(User)
+    time_taken = models.DateTimeField()
+
+
+SURVEY_LANGUAGES = (
+    ('ENG', 'English'),
+    ('TAM', 'Tamil')
+)
+
+
+class Survey(models.Model):
+    survey_id = models.AutoField(primary_key=True)
+    language = models.CharField(choices=SURVEY_LANGUAGES)
+
+
+class Answer(models.Model):
+    answer_id = models.AutoField(primary_key=True)
+    question_id = models.ForeignKey(Question)
+    survey_response_id = models.ForeignKey(SurveyResponse)
+    text_answer = models.TextField(max_length=2048)
+    bool_answer = models.BooleanField()
+    rate_answer = models.IntegerField()
+
+
+class SurveyQuestionMap(models.Model):
+    id = models.AutoField(primary_key=True)
+    question_id = models.ForeignKey(Question)
+    survey_id = models.ForeignKey(Survey)
+    text_question = models.TextField(max_length=1024)  # question text in the survey's language
+
+
+QUESTION_TYPES = (
+    ('BOOL', 'Boolean'),
+    ('RATE', 'Rating'),
+    ('TEXT', 'Text')
+)
+
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    answer_type = models.CharField(choices=QUESTION_TYPES)
+
+
+
+
+
+
+
+
+
+
