b90a987e4435ea6361530b82e11aaae0576acc23
Tanay Soni <tanaysoni12@gmail.com>
2018-01-27 16:47:24 +0530

    update model choices and null constraints

--- know_your_ward/surveys/models.py
+++ know_your_ward/surveys/models.py
@@ -11,49 +11,54 @@ class User(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
 
 
-SURVEY_LANGUAGES = (
-    ('ENG', 'English'),
-    ('TAM', 'Tamil')
-)
-
-
 class Survey(models.Model):
-    survey_id = models.AutoField(primary_key=True)
+    ENG = 'English'
+    TAM = 'Tamil'
+
+    SURVEY_LANGUAGES = (
+        (ENG, 'English'),
+        (TAM, 'Tamil')
+    )
+    survey = models.AutoField(primary_key=True)
     language = models.CharField(choices=SURVEY_LANGUAGES, max_length=5)
 
 
 class SurveyResponse(models.Model):
-    survey_id = models.ForeignKey(Survey)
-    user_id = models.ForeignKey(User)
-    time_taken = models.DateTimeField()
-
-
-QUESTION_TYPES = (
-    ('BOOL', 'Boolean'),
-    ('RATE', 'Rating'),
-    ('TEXT', 'Text')
-)
+    survey = models.ForeignKey(Survey)
+    user = models.ForeignKey(User)
 
 
 class Question(models.Model):
+    BOOL = 'BL'
+    RATE = 'RT'
+    TEXT = 'TX'
+
+    QUESTION_TYPES = (
+        (BOOL, 'Boolean'),
+        (RATE, 'Rating'),
+        (TEXT, 'Text')
+    )
+
     question_id = models.AutoField(primary_key=True)
     answer_type = models.CharField(choices=QUESTION_TYPES, max_length=5)
 
 
 class Answer(models.Model):
     answer_id = models.AutoField(primary_key=True)
-    question_id = models.ForeignKey(Question)
-    survey_response_id = models.ForeignKey(SurveyResponse)
-    text_answer = models.TextField(max_length=2048)
-    bool_answer = models.BooleanField()
-    rate_answer = models.IntegerField()
+    question = models.ForeignKey(Question)
+    survey_response = models.ForeignKey(SurveyResponse)
+    text_answer = models.TextField(max_length=2048, null=True, blank=True)
+    bool_answer = models.NullBooleanField(null=True, blank=True)
+    rate_answer = models.IntegerField(null=True, blank=True)
 
 
 class SurveyQuestionMap(models.Model):
     id = models.AutoField(primary_key=True)
-    question_id = models.ForeignKey(Question)
-    survey_id = models.ForeignKey(Survey)
+    question = models.ForeignKey(Question)
+    survey = models.ForeignKey(Survey)
     text_question = models.TextField(max_length=1024)  # question text in the survey's language
+
+
 
 
 
