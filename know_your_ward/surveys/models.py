fb5f85b665dccfe52749943216462d06e0c9527c
Tanay Soni <tanaysoni12@gmail.com>
2018-03-17 10:42:41 +0530

    `Answer` now referes to SurveyQuestionMap

--- know_your_ward/surveys/models.py
+++ know_your_ward/surveys/models.py
@@ -24,8 +24,8 @@ class Survey(models.Model):
 
 
 class SurveyResponse(models.Model):
-    survey = models.ForeignKey(Survey)
-    user = models.ForeignKey(User)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Question(models.Model):
@@ -43,20 +43,22 @@ class Question(models.Model):
     answer_type = models.CharField(choices=QUESTION_TYPES, max_length=5)
 
 
+class SurveyQuestionMap(models.Model):
+    id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    text_question = models.TextField(max_length=1024)  # question text in the survey's language
+
+
 class Answer(models.Model):
     answer_id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question)
-    survey_response = models.ForeignKey(SurveyResponse)
+    question = models.ForeignKey(SurveyQuestionMap, on_delete=models.CASCADE)
+    survey_response = models.ForeignKey(SurveyResponse, on_delete=models.CASCADE)
     text_answer = models.TextField(max_length=2048, null=True, blank=True)
     bool_answer = models.NullBooleanField(null=True, blank=True)
     rate_answer = models.IntegerField(null=True, blank=True)
 
 
-class SurveyQuestionMap(models.Model):
-    id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question)
-    survey = models.ForeignKey(Survey)
-    text_question = models.TextField(max_length=1024)  # question text in the survey's language
 
 
 
