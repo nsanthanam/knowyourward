703f81b154331e1f8fd933bec3d5d55ed9636427
Tanay Soni <tanaysoni12@gmail.com>
2018-03-24 12:51:55 +0530

    add query param for ward based filter of responses

--- know_your_ward/surveys/serializers.py
+++ know_your_ward/surveys/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers
+from collections import Counter
 
 from .models import SurveyQuestionMap
 
@@ -13,12 +14,17 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ('text_question', 'survey', 'question', 'responses')
 
     def get_responses(self, obj):
-        from collections import Counter
+        request = self.context['request']
+        ward = request.query_params.get('ward', None)
+        if ward:
+            queryset = obj.answer_set.filter(survey_response__user__ward=ward)
+        else:
+            queryset = obj.answer_set.all()
 
-        response = Counter([answer.bool_answer for answer in obj.answer_set.all()])
+        if obj.question.answer_type == 'BL':
+            response = Counter([answer.bool_answer for answer in queryset])
+        elif obj.question.answer_type == 'RT':
+            response = Counter([answer.rate_answer for answer in queryset])
+        else:
+            response = {}
         return response
-
-
-class QuestionViewSet(viewsets.ModelViewSet):
-    queryset = SurveyQuestionMap.objects.all()
-    serializer_class = QuestionSerializer
